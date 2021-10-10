dcfa3e6bb971b5fe617665457af0259d9b9e0e6f
Poza Radu <poza.radu@gmail.com>
2021-10-10 23:59:42 +0300

    Added greeting

---
+++ src/main/java/org/prg/rem/MainApp.java
@@ -0,0 +1,10 @@
+package org.prg.rem;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    public void start(Stage primaryStage) {
+        System.out.println("ceva");
+    }
+}
